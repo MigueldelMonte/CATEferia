cb08aff9d44d1648a42804c18bfd7649199ff62e
APVGITHUB <49819845+APVGITHUB@users.noreply.github.com>
2019-12-15 19:33:08 +0100

    Update MainActivity.java

--- DESARROLLO/java/MainActivity.java
+++ DESARROLLO/java/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.cateferia;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.Button;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button sign, login;
+    private Button sign, login_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sign = (Button)findViewById(R.id.button_sign);
-        login = (Button)findViewById(R.id.button_login);
+        login_1= (Button)findViewById(R.id.button_login_1);
+
+        login_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
